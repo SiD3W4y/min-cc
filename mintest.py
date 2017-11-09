2a29b56019a4b8ca8a86e0af4989359903daf024
Red Eclipse <noreply@redeclipse.net>
2017-11-09 21:47:58 +0100

    Added unit tests

---
+++ mintest.py
@@ -0,0 +1,6 @@
+import unittest
+
+from tests.assembler import AssemblyTest
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/assembler.py
@@ -0,0 +1,31 @@
+import unittest
+
+from min.assembly.MinInstruction import *
+from min.assembly.Assembler import *
+
+import min.data.ops
+
+
+class AssemblyTest(unittest.TestCase):
+
+    def test_instruction_comparison(self):
+        a1 = MinInstruction(0x2,OpArg(ArgType.ARG_REG,0),OpArg(ArgType.ARG_REG,0))
+        a2 = MinInstruction(0x2,OpArg(ArgType.ARG_REG,0),OpArg(ArgType.ARG_REG,0))
+        
+        return self.assertEqual(a1,a2)
+
+    def test_assemble_noarg(self):
+        asm = Assembler()
+        a2 = MinInstruction(ops.OP_RET,OpArg(ArgType.ARG_REG,0),OpArg(ArgType.ARG_REG,0))
+
+        return self.assertEqual(a2,asm.assembleInst("ret"))
+    
+    def test_assemble_single_arg1(self):
+        asm = Assembler()
+        a2 = MinInstruction(ops.OP_CALL,OpArg(ArgType.ARG_REG,1),OpArg(ArgType.ARG_REG,0))
+
+        return self.assertEqual(a2,asm.assembleInst("call $B"))
+
+
+if __name__ == '__main__':
+    unittest.main()
